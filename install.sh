8ac7201305f77cc0c729a70750ab5140778e3b73
Jerry_4539 <91048755+Jerry4539@users.noreply.github.com>
2021-10-03 10:11:59 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#installation
+
+clear
+
+echo ""
+echo ""
+echo "" SUBSCRIBE MY CHANNEL ITS FOR GAMES BUT ILL CREATE ONE FOR HACKING SOON🔔 ""
+xdg-open https://youtube.com/channel/UCRxQi8feV27FZkCwYDMyX-w
+echo ""
+
+echo -e "\e[101m Press Enter \e[0m"
+read a1
+
+sleep 1
+clear
+
+echo "Loading..."
+sleep 1
+clear
+
+echo "Loading..."
+sleep 1
+clear
+
+echo "Loading..."
+sleep 1
+clear
+
+toilet -f future "Jerry4539 "
+echo "-------------------------------------------------------"
+echo   "Created  : Jerry4539 ⁩ $white"
+echo   "INSTAGRAM  : Xhacking_official $white"
+echo   "website  : https://github.com/Jerry4539 $white"
+echo "-------------------------------------------------------"
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "Detected, Trying To Exit  ... "
+echo ""
+sleep 1
+echo ""
+echo "Jerry4539 "
+sleep 1
+exit
+}
+
+apt update
+apt upgrade -y
+pkg install ruby -y 
+gem install toilet -y 
+pkg install lolcat
+pkg install figlet
+apt install git php openssh curl -y
+pkg install python2 -y 
+pkg install nano php -y
+pip install mechanize
+pip install --upgrade pip
+
+echo ""
+echo "code_by_Jerry4539"
+echo "________________________"
